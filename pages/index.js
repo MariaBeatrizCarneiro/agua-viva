57fd13590e3edbde7d4e66106967555793092ef6
MariaBeatrizCarneiro <up202107798@up.pt>
2024-03-13 22:55:03 +0000

    homepage

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,21 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
+import { Inter } from 'next/font/google';
+import Link from "next/link";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   return (
-    <main className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}>
-      <p>Hello</p>
+    <main className="bg-cover bg-center bg-no-repeat" style={{ backgroundImage: `url('/fundo-mar.png')` }}>
+      <div className="min-h-screen flex flex-col justify-center items-center">
+      <div className="   ">
+          <h1 className="text-white text-5xl font-extralight tracking-widest">ÁGUA VIVA</h1>
+          <h2 className="text-white text-xl font-extralight tracking-widest">SURF ACADEMY</h2>
+      </div>
+        <Link href={"/classes"} passHref>
+          <p className="p-5 px-11 mt-20 bg-yellow text-darkBlue text-xl font-extralight tracking-widest rounded-xl">Iniciar Sessão</p>
+        </Link>
+        <h3 className="ms-12 text-white text-xs font-extralight tracking-widest">Esqueceu-se da password?</h3>
+      </div>
     </main>
   );
 }
