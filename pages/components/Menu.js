8e76f5c97e1f6f16a6e54b79a8b9cf054cd48b3d
MariaBeatrizCarneiro <up202107798@up.pt>
2024-03-13 22:56:01 +0000

    inicial menu

---
+++ pages/components/Menu.js
@@ -0,0 +1,28 @@
+import Link from "next/link";
+import { MdSurfing } from "react-icons/md";
+import { FaUserCircle } from "react-icons/fa";
+import { RiCalendarEventFill } from "react-icons/ri";
+import { MdDiscount } from "react-icons/md";
+
+export default function Menu() {
+  return (
+    <nav className="flex justify-between items-center bg-darkBlue px-8 py-3">
+      <Link className="text-white font-bold p-2" href={"/events"}>
+        <RiCalendarEventFill size={40}/>
+        Eventos
+      </Link>
+      <Link className="text-white font-bold p-2" href={"/coupons"}>
+        <MdDiscount size={40}/>
+        Cupões
+      </Link>
+      <Link className="text-white font-bold p-2" href={"/classes"}>
+        <MdSurfing size={40}/>
+        Aulas
+      </Link>
+      <Link className="text-white font-bold p-2" href={"/profile"}>
+        <FaUserCircle size={40}/>
+        Perfil
+      </Link>
+    </nav>
+  );
+}
