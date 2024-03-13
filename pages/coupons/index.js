f4e5fc7b88cd7da78e36a6466c2474773c030b91
MariaBeatrizCarneiro <up202107798@up.pt>
2024-03-13 23:12:10 +0000

    Navbar and Menu locally on pages

--- pages/coupons/index.js
+++ pages/coupons/index.js
@@ -1,11 +1,18 @@
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
+import Navbar from "../components/Navbar";
+import Menu from "../components/Menu";
 
 export default function Coupons() {
   return (
-    <main className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}>
-      <p>Página de cupões</p>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="top-0 fixed w-full">
+        <Navbar />
+      </div>
+      <div className="my-24 mx-4 flex-grow">
+        <p>Página de cupões</p>
+      </div>
+      <div className="bottom-0 fixed w-full">
+          <Menu />
+      </div>
     </main>
   );
 }
