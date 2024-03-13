af5761db167801fdf9fb27bb7f3aad4d8d5e5710
MariaBeatrizCarneiro <up202107798@up.pt>
2024-03-13 23:18:16 +0000

    edit text size

--- pages/index.js
+++ pages/index.js
@@ -9,12 +9,12 @@ export default function Home() {
       <div className="min-h-screen flex flex-col justify-center items-center">
       <div className="   ">
           <h1 className="text-white text-5xl font-extralight tracking-widest">ÁGUA VIVA</h1>
-          <h2 className="text-white text-xl font-extralight tracking-widest">SURF ACADEMY</h2>
+          <h2 className="text-white text-2xl font-extralight tracking-widest">SURF ACADEMY</h2>
       </div>
         <Link href={"/classes"} passHref>
-          <p className="p-5 px-11 mt-20 bg-yellow text-darkBlue text-xl font-extralight tracking-widest rounded-xl">Iniciar Sessão</p>
+          <p className="p-5 px-11 mt-20 bg-yellow text-darkBlue text-2xl font-extralight tracking-widest rounded-xl">Iniciar Sessão</p>
         </Link>
-        <h3 className="ms-12 text-white text-xs font-extralight tracking-widest">Esqueceu-se da password?</h3>
+        <h3 className="ms-14 mt-2 text-white text-xs font-extralight tracking-widest">Esqueceu-se da password?</h3>
       </div>
     </main>
   );
