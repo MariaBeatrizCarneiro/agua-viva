02b9c2141c62d2aafc8346f92df39264fe8c1fcb
Vanessa Silva <156242127+vanessapnk@users.noreply.github.com>
2024-03-14 10:43:38 +0000

    icon main page

--- pages/components/Menu.js
+++ pages/components/Menu.js
@@ -6,20 +6,20 @@ import { MdDiscount } from "react-icons/md";
 
 export default function Menu() {
   return (
-    <nav className="flex justify-between items-center bg-darkBlue px-8 py-3">
-      <Link className="text-white font-bold p-2" href={"/events"}>
+    <nav className="flex justify-between font-extralight items-center bg-darkBlue px-8 py-3">
+      <Link className="text-white p-2" href={"/events"}>
         <RiCalendarEventFill size={40}/>
         Eventos
       </Link>
-      <Link className="text-white font-bold p-2" href={"/coupons"}>
+      <Link className="text-white p-2" href={"/coupons"}>
         <MdDiscount size={40}/>
         Cupões
       </Link>
-      <Link className="text-white font-bold p-2" href={"/classes"}>
+      <Link className="text-white p-2" href={"/classes"}>
         <MdSurfing size={40}/>
         Aulas
       </Link>
-      <Link className="text-white font-bold p-2" href={"/profile"}>
+      <Link className="text-white p-2" href={"/profile"}>
         <FaUserCircle size={40}/>
         Perfil
       </Link>
